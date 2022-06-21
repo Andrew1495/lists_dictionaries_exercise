d4d91aa468884eefd82c9872d3b6ee74b887d8ab
Andrew <andrewfairley95@gmail.com>
2022-06-21 19:07:38 +0100

    finshed exc D 1,2,3

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,10 +3,11 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-
-# for number in numbers:
-#     if number/number 
-
+even_int = []
+for number in numbers:
+    if number % 2 == 0 and isinstance(number, int):
+        even_int.append(number)
+print(even_int)
 
 # 2. Print the difference between the largest and smallest value:
 
@@ -15,28 +16,24 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 # print(number_gap)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-
+last = None
 for number in numbers:
-    if number == 2 and number1 == 2:
+    if number == 2 and last == 2:
         print(True)
-
-    else:
-        print(False)
-
+    else: 
+        last = number
+       
 
 
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
-#    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
-
 
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
 #    And numbers that come immediately after a 13 also do not count.
 #    HINT - You will need to track the index throughout the loop.
-#
 #    So [5, 13, 2] would have sum of 5. 
 
 
